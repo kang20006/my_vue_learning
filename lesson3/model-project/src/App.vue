243721c04a6cafc42ad9509dcfa99caf1c1ab336
kang20006 <kang20006@gmail.com>
2022-04-19 11:31:05 +0800

    lesson4

--- lesson3/model-project/src/App.vue
+++ lesson3/model-project/src/App.vue
@@ -1,9 +1,30 @@
 <template>
   <h1>{{title}}</h1>
+  <p>Welcome...</p>
   <input type="text" ref="name">
   <button @click="handleClick">click me</button>
   <!-- all the template in the modal will place here -->
-  <MyModal/>
+  <teleport to=".modals" v-if="showModal">
+    <MyModal :header="header" :text="text" theme="sale" @close="toggleModal">
+      <!-- name slot -->
+      <template v-slot:links>
+        <a href="a">sign up now</a>
+        <a href="a">more info</a>
+      </template>
+      <!-- default slot -->
+      <h1>slots111</h1>
+      <p>slots test</p>
+    </MyModal>
+  </teleport>
+  <teleport to=".modals" v-if="showModalTwo">
+    <MyModal @close="toggleModalTwo">
+      <!-- default slot -->
+      <h1>Sign up to the newsletter</h1>
+      <p>For updates and promo codes!</p>
+    </MyModal>
+  </teleport>
+  <button @click="toggleModal">open modal</button>
+  <button @click="toggleModalTwo">open modal two</button>
 </template>
 
 <script>
@@ -16,7 +37,11 @@ export default {
   name: 'App',
   data(){
     return {
-      title: 'My First Vue App :)'
+      title: 'My First Vue App :)',
+      header: 'Sign up for the Giveaway!',
+      text: 'Grab your food for half prize!',
+      showModal:false,
+      showModalTwo:false,
     }
   },
   methods:{
@@ -28,6 +53,12 @@ export default {
       // focus the input element
       this.$refs.name.focus()
 
+    },
+    toggleModal(){
+      this.showModal=!this.showModal
+    },
+    toggleModalTwo(){
+      this.showModalTwo=!this.showModalTwo
     }
   },
   // register any extra component we use in this 
@@ -42,7 +73,7 @@ export default {
 // apply to any component in the page
 // style are place on the head of index.html
 <style>
-#app {
+#app .modal{
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
--- lesson3/model-project/src/components/Modal.vue
+++ lesson3/model-project/src/components/Modal.vue
@@ -1,8 +1,16 @@
 <template>
-    <div class="backdrop"> 
-        <div class="modal">
-            <h1>Modal Title</h1>
-            <p>modal content</p>
+    <div class="backdrop" @click.self="closeModal"> 
+        <!-- if theme equal to sale then give class =sale -->
+        <div class="modal" :class="{sale:theme==='sale'}">
+            <h1>{{ header }}</h1>
+            <p>{{text}}</p>
+            <!-- will pass into everything inside tag <MyModal><MyModal/> -->
+            <!-- "default contnent" will only show if we didnt pass in any content -->
+            <slot>default contents</slot>
+            <!-- pass slot with name links into slot -->
+            <div class="actions">
+                <slot name="links"></slot>
+            </div>
         </div>
     </div>
 </template>
@@ -10,6 +18,15 @@
 <script>
 export default {
   name: "MyModal",
+  props:['header','text','theme'],
+  methods:{
+      closeModal(){
+          this.$emit('close')
+      }
+  },
+  data(){
+      return{}
+  }
 };
 </script>
 
@@ -40,8 +57,35 @@ export default {
     color:#03cfb4;
     border:none;
     padding: 0;
-} 
+  
+}
+.modal.sale{
+    background: crimson;
+    color: white;
 
+}
+.modal.sale h1{
+    color: white;
+}
+.modal .actions{
+    text-align: center;
+    margin: 30px 0 10px 0;
+    color:#333;
+}
+.modal .actions a{
+    color:#333;
+    padding:8px;
+    border:1px solid #eee;
+    border-radius:4px;
+    text-decoration: none;
+    margin:10px; 
+}
+.modal.sale .actions{
+    color:white;
+}
+.modal.sale .actions a{
+    color:white;
+}
 </style>
 
 // apply style only for this component (scoped)
