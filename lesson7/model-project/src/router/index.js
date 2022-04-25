6e58f027098ab2433f6336ff75322a73158e1c31
kang20006 <kang20006@gmail.com>
2022-04-25 13:03:51 +0800

    lesson7

---
+++ lesson7/model-project/src/router/index.js
@@ -0,0 +1,48 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+import AboutView from '../views/AboutView.vue'
+import JobsView from '../views/jobs/JobsView.vue'
+import JobDetails from '../views/jobs/JobDetails.vue'
+import NotFound from '../views/NotFound.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    name: 'about',
+    component: AboutView
+  },
+  {
+    path:'/jobs',
+    name: 'jobs',
+    component:JobsView
+  },
+  {
+    path: '/jobs/:id',
+    name: 'jobdetails',
+    component:JobDetails,
+    props: true
+  },
+  //redirect
+  {
+    path:'/all-jobs',
+    redirect:'/jobs'
+  },
+  //catchall 404
+  {
+    path:'/:catchAll(.*)',
+    name: 'NotFound',
+    component: NotFound,
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
---
+++ lesson7/model-project/src/views/HomeView.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="home">
+    <h1>Hello</h1>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Perferendis ipsa dolor ea illo ab quos odit delectus tempora exercitationem magnam totam maiores, dignissimos eligendi iste atque nisi illum velit deserunt? Quod ut quisquam, necessitatibus sapiente beatae deleniti quibusdam magnam voluptas odit deserunt maxime eligendi vitae accusantium accusamus fugiat recusandae non fuga doloremque provident rem praesentium unde. Consequatur sequi, ad voluptatum deleniti exercitationem alias iure dolor! Mollitia quisquam, eaque odio veritatis harum vitae inventore fugit molestias doloremque doloribus blanditiis nemo perferendis aliquid voluptatum iste molestiae porro? Corporis, nulla blanditiis repellendus tenetur voluptas aut molestias exercitationem labore harum nobis iusto officia pariatur.</p>
+    <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Officiis a maiores reiciendis ea dignissimos debitis itaque suscipit fuga blanditiis, natus unde quis, dolor eveniet asperiores, est quisquam molestiae adipisci sequi magnam. Hic temporibus, eligendi accusamus quasi iste veritatis minus facilis explicabo quo praesentium, commodi dolor. Ipsam ducimus perferendis corporis magnam blanditiis, accusamus possimus atque quaerat minima fuga modi aliquid expedita libero deserunt iusto, quibusdam in cumque neque vel saepe autem est quidem nostrum commodi? Molestias quidem delectus beatae, nesciunt totam fugiat veritatis nisi atque non, ab corporis nihil, omnis error in quibusdam. Odio animi vel illo eveniet mollitia adipisci veniam?</p>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name:'Home',
+}
+</script>
